9e46f5b2cd3b0454cf1daf9704eac3b9f6c7108f
root <root@localhost.localdomain>
2014-02-09 00:26:08 +0200

    Added/fixed more build scripts

---
+++ scripts/build/install_deps.sh
@@ -0,0 +1,17 @@
+# TODO: Needs cleaning
+apt-get install \
+    libgd2-dev pcregrep  \
+    libpcre3 libpcre3-dev \
+    libxslt \
+    libxslt1-dev libxslt1.1 \
+    libgd-dev \
+    libgd2-dev \
+    libgd2-xpm-dev \
+    geoip-bin \
+    geoip-database \
+    geoclue \
+    geoip-database \
+    geoip \
+    libgeoip-dev \
+    libcr-dev \
+    libperl-dev
--- scripts/build/install_php_mongo.sh
+++ scripts/build/install_php_mongo.sh
@@ -4,4 +4,27 @@
 
 echo Installing Mongo adapter for Php
 
-# TODO: finish this
+if [[ $(php -m) == *mongo* ]]
+then
+    echo Php already has Mongo driver
+    #exit;
+fi  
+
+tmpDir="/tmp/php_mongo/"
+mkdir -p $tmpDir
+
+cd $tmpDir/
+
+pmGit="https://github.com/mongodb/mongo-php-driver/archive/master.zip"
+wget $pmGit
+unzip master.zip
+cd mongo-php-driver-master/
+
+phpize
+./configure
+make
+make install
+
+echo "extension=mongo.so" > /etc/php/conf.d/mongo.ini
+
+rm -rf $tmpDir
