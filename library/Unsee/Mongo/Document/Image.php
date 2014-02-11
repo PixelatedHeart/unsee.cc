48a1798b5dc65a0ea9d8ef9c451aff4724422dc9
Michael Gorianskii <goreanski@gmail.com>
2014-02-11 22:43:19 +0200

    Watermarking using GD2 instead of Imagick

--- library/Unsee/Mongo/Document/Image.php
+++ library/Unsee/Mongo/Document/Image.php
@@ -35,56 +35,23 @@ class Unsee_Mongo_Document_Image extends Shanty_Mongo_Document
 
     public function watermark()
     {
-        // Create objects
-        $image = $this->getImaick();
-
-        // Watermark text
-        $watermark = new Imagick();
         $text = $_SERVER['REMOTE_ADDR'];
+        $image = imagecreatefromstring($this->data->bin);
+        $font = 'pixel.ttf';
+        $im = imagecreatetruecolor(800, 800);
 
-        // Create a new drawing palette
-        $draw = new ImagickDraw();
-        $watermark->newImage(240, 80, new ImagickPixel('none'));
+        imagesavealpha($im, true);
+        imagefill($im, 0, 0, imagecolorallocatealpha($im, 0, 0, 0, 127));
+        imagettftext($im, 12, 0, 200, 200, -imagecolorallocatealpha($im, 255, 255, 255, 80), $font, $text);
+        imagealphablending($im, true);
+        imagesettile($image, $im);
+        imagefilledrectangle($image, 0, 0, imagesx($image), imagesy($image), IMG_COLOR_TILED);
 
-        // Set font properties
-        $draw->setFont('Courier');
-        $draw->setFillColor('White');
-        $draw->setfontsize(30);
-        $draw->setFillOpacity(.4);
+        ob_start();
+        imagejpeg($image);
 
-        // Position text at the top left of the watermark
-        $draw->setGravity(Imagick::GRAVITY_NORTHWEST);
-
-        // Draw text on the watermark
-        $watermark->annotateImage($draw, 10, 10, 0, $text);
-
-        // Position text at the bottom right of the watermark
-        $draw->setGravity(Imagick::GRAVITY_SOUTHEAST);
-
-        // Repeatedly overlay watermark on image
-        for ($w = 0; $w < $image->getImageWidth(); $w += 600) {
-            for ($h = 0; $h < $image->getImageHeight(); $h += 600) {
-                $image->compositeImage($watermark, Imagick::COMPOSITE_OVER, $w, $h);
-            }
-        }
-
-        list(, $format) = explode('/', $this->type);
-
-        // Set output image format
-        $image->setImageFormat($format);
-
-        /*
-        $comment = 'The image was not intended for sharing, ' .
-                'but was foully taken from https://www.unsee.cc/ ' .
-                'on ' . date('c') . '.' . PHP_EOL .
-                'Below is info on the bad person:' . PHP_EOL .
-                'IP: ' . $_SERVER['REMOTE_ADDR'] . PHP_EOL .
-                'User agent: ' . $_SERVER['HTTP_USER_AGENT'] . PHP_EOL;
-
-        $image->commentimage($comment);
-         */
-        $this->data = new MongoBinData($image->getimageblob());
-        $this->size = $image->getimagesize();
+        $this->data = new MongoBinData(ob_get_clean());
+        $this->size = strlen($this->data);
 
         return true;
     }
