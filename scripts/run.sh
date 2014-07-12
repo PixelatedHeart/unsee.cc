d1f22a9cba194db1857206c58d9013441e17a153
Michael Gorianskii <goreanski@gmail.com>
2014-07-12 21:05:49 +0300

    Added supervisor

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)/../;
 git fetch && git reset --hard origin/master;
 rm application/configs/env.php;
 killall node;
-nohup node scripts/chat.js > scripts/output.log &
+nohup supervisor scripts/chat.js > scripts/output.log &
